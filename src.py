b3aadff62303632e0e4d08d72cac84e1771a0a00
Caroline Gandolfi <63135685+CarolGandolfi@users.noreply.github.com>
2023-11-09 21:54:25 -0300

    Update src.py

--- src.py
+++ src.py
@@ -4,32 +4,37 @@ import RPi.GPIO as gpio
 import time
 import aiohttp
 
+#Pins de input do sinal da boia
 PIN1=00   # Usando modo BCM
 PIN2=07
 
+#Responsável por mandar anotificação no telegram
 async def sendToTelegram(message):
     bot = telegram.Bot(token='6484292851:AAG0odJEkLZ33PbZi7uxrFmkNuDEXuhHdmU')
     await bot.send_message(chat_id='-4067332732', text=message)
 
- 
+#Responsável por ouvir os inputs e realizar as devidas ações 
 def gpioListener(pin1, pin2):
     while True:
-        if gpio.input(pin1) == gpio.HIGH:
+        if  cavalo==0 & gpio.input(pin1) == gpio.HIGH:
             asyncio.run(sendToTelegram('Nível do galão baixo... Convém reabastecer!'))
             asyncio.run(http_request_ifttt('galao_vazio'))
+            cavalo=1
 
-        else:
+        elif cavalo==1 & gpio.input(pin1) == gpio.LOW:
             asyncio.run(sendToTelegram('Galão reabastecido!!'))
+            cavalo=0
 
         if gpio.input(pin2) == gpio.HIGH:
             asyncio.run(http_request_ifttt('reservatorio_cafeteira'))
 
         time.sleep(1)
 
-
+#Função main que chama o listener
 def main():
     gpioListener(PIN1, PIN2)
 
+#Função que faz a chamada HTTP para monitoramento de uso da cafeteira
 async def http_request_ifttt(event_name):
     try:
         url = "https://maker.ifttt.com/trigger/cafeteira_event/with/key/fSRoSbSiX_JywuqSNYyDUBBDHXWq2Y02k1Yd4Tm1uzC"
@@ -57,5 +62,3 @@ main()
 
 gpio.cleanup()
 exit()
-
-
